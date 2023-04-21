15b87616dd32d71bb55cc783a6bb207d1db00827
Robert Marchino <rmarchino95@gmail.com>
2023-04-21 18:55:48 -0500

    commit two

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -35,7 +35,7 @@ const Formulario = ({ setMonedas }) => {
 
     useEffect(() => {
         const consultarAPI = async () => {
-            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=50&tsym=USD" 
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD" 
             const respuesta = await fetch(url);
             const resultado = await respuesta.json();
             
