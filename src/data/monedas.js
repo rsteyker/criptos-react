fd5e56d868a2f8f2fdfa1f589408a627b97b8de9
Robert Marchino <rmarchino95@gmail.com>
2023-04-21 18:48:04 -0500

    commit quitar sol peruano

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -3,7 +3,6 @@ const monedas = [
     {id: 'MXN', nombre: 'Peso Mexicano'},
     {id: 'EUR', nombre: 'Euro'},
     {id: 'GBP', nombre: 'Libra Esterlina'},
-    {id: 'S/', nombre: 'Nuevo Sol peruano'}
 ]
 
 export {
